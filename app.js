f6d9d6df55f103756ebe763bdac3c558558fb031
Mattias Knutsson <matknu@kth.se>
2017-01-30 20:42:07 +0100

    Getting there

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+var exphbs = require('express-handlebars');
 const http = require('http');
 const parseXmlStr = require('xml2js').parseString
 function getDate(time) {
@@ -18,24 +19,23 @@ function formatDate(date) {
   str += ':' + ('0' + date.getMinutes()).slice(-2);
   return str
 }
+
 function fetchData(option, resolve, reject) {
   http.get(option, res => {
     res.setEncoding('utf8');
     let rawData = '';
     res.on('data', (chunk) => rawData += chunk);
+    res.on('error', reject)
     res.on('end', () => {
       try {
         parseXmlStr(rawData, (err, res) => {
-          resolve(res.tv.programme.filter((program) => {
+          var data = res.tv.programme.filter((program) => {
             program.$.stopDate = getDate(program.$.stop);
             program.$.startDate = getDate(program.$.start);
             return program.$.stopDate > new Date();
-          }).map(item => {
-            // resolve(item);
-            // console.log(`${formatDate(item.$.startDate)}\t${item.title[0]._}`);
-            return item;
-          }));
-        });
+          })
+          resolve({ channel: data[0].$.channel, program: data.map(programMapper) });
+        })
 
       } catch (e) {
         reject(e.message);
@@ -43,20 +43,37 @@ function fetchData(option, resolve, reject) {
     });
   })
 }
+function programMapper(program) {
+  return {
+    start: program.$.startDate.toTimeString().substr(0, 5),
+    end: program.$.stopDate.toTimeString().substr(0, 5),
+    title: program.title[0]._,
+    desc: program.desc && program.desc[0]._,
+    category: program.category.map(item => item._)
+
+  }
+}
 
 const getOption = (channel, date) => `http://xmltv.xmltv.se/${channel}_${date}.xml`;
 
-let options = [
-  getOption('svt1.svt.se', new Date().toISOString().split('T')[0])
-];
-
-var promises = options.map(option => {
-  return new Promise(function (resolve, reject) {
-    fetchData(option, resolve, reject);
+express()
+  .engine('handlebars', exphbs({ defaultLayout: 'main' }))
+  .set('view engine', 'handlebars')
+  .use('/static', express.static('static'))
+  .get('/', (req, res) => {
+    let options = [
+      getOption('svt1.svt.se', new Date().toISOString().split('T')[0]),
+      getOption('svt2.svt.se', new Date().toISOString().split('T')[0]),
+      getOption('tv4.se', new Date().toISOString().split('T')[0])
+    ];
+    var promises = options.map(option => {
+      return new Promise(function (resolve, reject) {
+        fetchData(option, resolve, reject);
+      })
+    });
+    Promise.all(promises).then(data => {
+      console.log(data)
+      res.render('home', { data });
+    })
   })
-});
-Promise.all(promises).then(data => {
-  console.log(data[0].length);
-})
-// promises[0].then(data => console.log('nja', data))
-console.log('promises', promises);
+  .listen(8081);
