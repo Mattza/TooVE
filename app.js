fa792915ba0beb1ce9b965cc69653953a1fa21e8
Mattias Knutsson <matknu@ppm.nu>
2017-01-31 12:29:45 +0100

    Add client-cache

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const parseXmlStr = require('xml2js').parseString;
 const getDate = time => new Date(Date.UTC(time.substr(0, 4), time.substr(4, 2) - 1, time.substr(6, 2), time.substr(8, 2) - 1, time.substr(10, 2)));
 const getProxyOption = (option, proxyUrl) => ({host: proxyUrl.substr(7, 13), port: 8080, path: option, headers: {host: 'xmltv.xmltv.se'}});
 
-let fetchData = (res, resolve, reject) => {
+let cache = {};
+
+const fetchData = (cacheKey, res, resolve, reject) => {
   let rawData = '';
   res.on('data', chunk => rawData += chunk);
   res.on('error', reject);
@@ -30,6 +32,7 @@ let fetchData = (res, resolve, reject) => {
               desc: program.desc && program.desc[0]._,
               category: program.category && program.category.map(item => item._)
             }));
+        cache[cacheKey] = {channel, program}
         resolve({channel, program});
       })
     } catch (e) {
@@ -43,13 +46,18 @@ const getOption = (channel, date) => {
   let url = `http://xmltv.xmltv.se/${channel}_${date}.xml`;
   return process.env.HTTP_PROXY ? getProxyOption(url, process.env.HTTP_PROXY) : url;
 };
-function get(req, res) {
 
+const get = (req, res) => {
   let date = (req.params.day ? new Date(req.params.day) : new Date()).toISOString().split('T')[0];
   let channels = ['svt1.svt.se', 'svt2.svt.se', 'tv4.se'];
 
   let promises = channels.map(channel => new Promise((resolve, reject) => {
-    http.get(getOption(channel, date), res => fetchData(res, resolve, reject))
+    let cacheKey = channel + date;
+    if (cache[cacheKey]) {
+      resolve(cache[cacheKey]);
+    } else {
+      http.get(getOption(channel, date), res => fetchData(cacheKey, res, resolve, reject))
+    }
   }));
   Promise.all(promises).then(data => {
     res.render('home', {data});
@@ -57,7 +65,7 @@ function get(req, res) {
     res.status(500);
     res.send(e);
   })
-}
+};
 express()
     .engine('handlebars', require('express-handlebars')({defaultLayout: 'main'}))
     .set('view engine', 'handlebars')
