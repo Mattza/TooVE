d8f7fc6554e1d1322c6597af42c51066e5785eb4
Mattias Knutsson <matknu@ppm.nu>
2017-03-06 12:53:19 +0100

    Add initial csr

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const http = require('http');
 const exphbs = require('express-handlebars');
-const parseXmlStr = require('xml2js').parseString
+const parseXmlStr = require('xml2js').parseString;
 
 const getDate = (time, addHour) => new Date(Date.UTC(
-  time.substr(0, 4), time.substr(4, 2) - 1, time.substr(6, 2), parseInt(time.substr(8, 2)) + addHour, time.substr(10, 2)));
+    time.substr(0, 4), time.substr(4, 2) - 1, time.substr(6, 2), parseInt(time.substr(8, 2)) + addHour, time.substr(10, 2)));
 const getUrl = (channel, date) => `http://xmltv.xmltv.se/${channel}_${date}.xml`;
 const programMapper = program => ({
   start: program.$.startDate.toTimeString().substr(0, 5),
@@ -13,7 +13,7 @@ const programMapper = program => ({
   title: program.title[0]._,
   desc: program.desc && program.desc[0]._,
   category: program.category && program.category.map(item => item._)
-})
+});
 
 let hbs = exphbs.create({
   defaultLayout: 'main',
@@ -22,54 +22,73 @@ let hbs = exphbs.create({
   }
 });
 
-function fetchData(option) {
+function fetchData(optionU) {
   return new Promise((resolve, reject) => {
-    http.get(option.url, res => {
+    var option = {
+      host: "proxyw.ppm.nu",
+      port: 8080,
+      path: optionU.url,
+      headers: {
+        host: "xmltv.xmltv.se"
+      }
+    };
+    http.get(option, res => {
       let rawData = '';
       res.on('data', (chunk) => rawData += chunk);
       res.on('error', reject)
       res.on('end', () => {
         parseXmlStr(rawData, (err, res) => {
-          var data = res.tv.programme.filter((program) => {
+          const data = res.tv.programme.filter((program) => {
             program.$.stopDate = getDate(program.$.stop, -1);
             program.$.startDate = getDate(program.$.start, 0);
             return program.$.stopDate > new Date();
-          })
-          resolve({ channel: option.display, program: data.map(programMapper) });
+          });
+          resolve({channel: option.display, program: data.map(programMapper)});
         })
       });
     })
   })
 }
 
+let getData = (date) => {
+  let dateStr = date.toISOString().split('T')[0];
+  let channels = [
+    {display: 'SVT1', code: 'svt1.svt.se'},
+    {display: 'SVT2', code: 'svt2.svt.se'},
+    {display: 'TV4', code: 'tv4.se'}
+  ];
+  channels.forEach(channel => channel.url = getUrl(channel.code, dateStr));
+  return Promise.all(channels.map(fetchData))
+};
 
 express()
-  .engine('handlebars', hbs.engine)
-  .set('view engine', 'handlebars')
-  .use(require('compression')({ level: 9 }))
-  .use('/static', express.static('static'))
-  .get('/', (req, res) => {
+    .engine('handlebars', hbs.engine)
+    .set('view engine', 'handlebars')
+    .use(require('compression')({level: 9}))
+    .use('/static', express.static('static'))
+    .get('/', (req, res) => {
 
-    let date = new Date();
-    let thisDay = '';
-    if (req.query.date && req.query.date !== date.toISOString().split('T')[0]) {
-      console.log(req.query.date);
-      date = new Date(req.query.date);
-      thisDay = date.getDate() + ' / ' + (date.getMonth() + 1);
-    }
-    let dateStr = date.toISOString().split('T')[0];
-    let channels = [
-      { display: 'SVT1', code: 'svt1.svt.se' },
-      { display: 'SVT2', code: 'svt2.svt.se' },
-      { display: 'TV4', code: 'tv4.se' }
-    ];
-    date.setDate(date.getDate() - 1)
-    let prevDay = date.toISOString().split('T')[0];
-    date.setDate(date.getDate() + 2)
-    let nextDay = date.toISOString().split('T')[0];
-    channels.forEach(channel => channel.url = getUrl(channel.code, dateStr));
-    Promise.all(channels.map(fetchData))
-      .then(data => res.render('home', { data, prevDay, nextDay, thisDay }))
-  })
-  .listen(process.env.PORT || 8081);
+      let date = new Date();
+      let thisDay = '';
+      if (req.query.date && req.query.date !== date.toISOString().split('T')[0]) {
+        date = new Date(req.query.date);
+        thisDay = date.getDate() + ' / ' + (date.getMonth() + 1);
+      }
+      date.setDate(date.getDate() - 1);
+      let prevDay = date.toISOString().split('T')[0];
+      date.setDate(date.getDate() + 2);
+      let nextDay = date.toISOString().split('T')[0];
+      getData(date).then(data => res.render('home', {data, prevDay, nextDay, thisDay}))
+    })
+    .get('/csr', (req,res) => {
+      res.render('csr')
+    })
+    .get('/api', (req, res) => { //http://localhost:8082/api?date=2017-03-08
+      let date = new Date();
+      if (req.query.date && req.query.date !== date.toISOString().split('T')[0]) {
+        date = new Date(req.query.date);
+      }
+      getData(date).then(data => res.send(data))
+    })
+    .listen(process.env.PORT || 8082);
 console.log('port 8081');
